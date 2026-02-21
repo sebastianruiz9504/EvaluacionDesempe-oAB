dc3b4db8853c375c82988f02716e9cb5abf414b7
Sebastian Ruiz <sruiz@digitaltechcolombia.com>
2026-02-21 15:16:36 -0500

    trigger deploy

--- obj/Debug/net8.0/EvaluacionDesempenoAB.AssemblyInfo.cs
+++ obj/Debug/net8.0/EvaluacionDesempenoAB.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvaluacionDesempenoAB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b4bdba0a1f5a4860b415c44d9e43b9f867fc6e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80bb9d7c628799d15b8e1951d5576109e4c3b387")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvaluacionDesempenoAB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvaluacionDesempenoAB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
