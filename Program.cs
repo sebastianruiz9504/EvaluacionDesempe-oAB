aa099e642eedf8a8b8c6b4e2720aca4bec1ce5ba
Sebastian Ruiz <sruiz@digitaltechcolombia.com>
2026-02-21 21:14:28 -0500

    azure7

--- Program.cs
+++ Program.cs
@@ -126,6 +126,10 @@ app.UseAuthorization();
 
 // Ruta por defecto MVC
 app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
